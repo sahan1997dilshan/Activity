f949b87bf0c8da77b9d8565d9e4a7454872c909c
sahan1997dilshan <dsahan548@gmail.com>
2021-05-14 10:41:59 +0530

    firat project

---
+++ quection.scala
@@ -0,0 +1,24 @@
+object quection extends App{
+    def convert(x:Double):Double=x*1.8+32;
+      println(convert(35)+"F");
+
+        def volume(y:Double):Double=22*y*y*y*4/(3*7);
+          println(volume(5)+"m^2");
+
+            def cal(x:Int):Double={
+                    if(x<=50){
+                              (24.95*x+3)*60/100;
+
+                                    }else{
+                                              (24.95*50+3+(x-50)*0.75)*60/100;
+
+
+                                                    }
+
+
+                                                      }
+              println(cal(60)+"rs");
+
+
+
+}
